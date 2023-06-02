7b7202e0d12b765b40fbbaef1c0fe6a5ed1e77f5
Jesus R. Hernandez Vera <jhernandez@csc.tranred.com.ve>
2023-06-02 12:29:46 -0400

    pagoproveedores bangete

--- variables.py
+++ variables.py
@@ -1,10 +1,18 @@
-server= "10.198.73.35"
-database = "milpagos"
+#qa
+# server= "10.198.73.35"
+# database = "milpagos"
+# afiliado = "milpagos"
+# username = "amendoza"
+# password = "Am1523246."
+
+#prod
+server= "10.198.71.45"
+database = "MilPagos"
 afiliado = "milpagos"
-username = "amendoza"
-password = "Am1523246."
+username = "usr_milpagos"
+password = "usr_milpagos"
 #
-rutaArchivo = "C:\\archivos\\Reportes_MilPagos\\BGENTE"
+rutaArchivo = r"C:\\archivos\\Reportes_MilPagos\\BGENTE"
 cuentaDebito = "0000001040107160107199659"
 reportBeginFile = 30
 nroAfiliado = "000000722000031"
